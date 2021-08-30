b441494820e2b1f48a379efa50ce2ac6e574b3cb
Issac Abraham <issaca32@gmail.com>
2021-08-30 15:31:51 +0100

    creates custom hook useFetch

--- react-blog/src/Home.js
+++ react-blog/src/Home.js
@@ -2,26 +2,16 @@ import React from 'react';
 import { useState } from 'react';
 import {useEffect} from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
     let name = "Mario"
-    const  [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-
-    useEffect(() => {
-        setTimeout(() =>{
-            fetch(" http://localhost:8000/blogs").then(response => {
-                return response.json();
-            }).then(data => {
-                setBlogs(data);
-                setIsPending(false);
-            })
-        }, 1000)
-    }, [])
+    const {data, isPending, error} = useFetch("http://localhost:8000/blogs")
     return (
         <div className="home">
+            { error && <div>{error}</div>}
             {isPending && <div>Loading</div>}
-            {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
+            {data && <BlogList blogs={data} title="All Blogs"/>}
         </div>
     );
 };
