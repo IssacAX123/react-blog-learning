783fc84b4197331944dca1bef3581e3ea78857e8
Issac Abraham <issaca32@gmail.com>
2021-08-30 13:01:55 +0100

    fetches initial data using json server

--- react-blog/src/Home.js
+++ react-blog/src/Home.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { useState } from 'react';
+import {useEffect} from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
+    let name = "Mario"
+    const  [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() =>{
+            fetch(" http://localhost:8000/blogs").then(response => {
+                return response.json();
+            }).then(data => {
+                setBlogs(data);
+                setIsPending(false);
+            })
+        }, 1000)
+    }, [])
     return (
         <div className="home">
-            <h1>Homepage</h1>
+            {isPending && <div>Loading</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
     );
 };
-
 export default Home;
